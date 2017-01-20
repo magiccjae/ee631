38ebd57cfcb790c826aa08d3f890c08e383db65e
Jae Lee <jaelee.byu@gmail.com>
2017-01-20 13:19:16 -0700

    hw1 task2

---
+++ hw1/feature_detect.cpp
@@ -0,0 +1,109 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**)
+{
+    VideoCapture video(0); // open the default camera
+    namedWindow("Lee", CV_WINDOW_AUTOSIZE);
+    cout << "Press the following number to choose operation:" << endl;
+    cout << "  1. Binarize" << endl;
+    cout << "  2. Canny Edges" << endl;
+    cout << "  3. Detect Corners" << endl;
+    cout << "  4. Detect Lines" << endl;
+    cout << "  5. Difference Image" << endl;
+    cout << "Press 'Q' to Quit" << endl;
+    int choice = 0;
+    Mat src, src_gray, dst, dst_norm, dst_norm_scaled, cdst, prev_frame;
+    // for binarize
+    int threshold_value = 128;
+    int max_binary_value = 255;
+    int threshold_type = 0;
+    // for canny
+    int lowThreshold = 30;
+    int ratio = 3;
+    int kernel_size = 3;
+    // for corner
+    int max_corners = 75;
+    vector<Point2f> corners;
+    double quality_level = 0.01;
+    double min_distance = 10;
+    int block_size = 3;
+    bool use_harris_detector = false;
+    double k = 0.04;
+    Size win_size = Size(5,5);
+    Size zero_zone = Size(-1,-1);
+    TermCriteria criteria = TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 );
+    // for line
+    int threshold_line = 130;
+    // for difference
+    bool initial_flag = true;
+    while(1){
+      video >> src;
+      int key = waitKey(10);
+      if(key != -1){
+        choice = key;
+      }
+      if(choice==49){
+        cvtColor(src, src_gray, CV_BGR2GRAY);
+        threshold(src_gray, dst, threshold_value, max_binary_value, threshold_type);
+        imshow("Lee", dst);
+      }
+      else if(choice==50){
+        cvtColor(src, src_gray, CV_BGR2GRAY);
+        blur(src_gray, src_gray, Size(3,3));
+        Canny(src_gray, dst, lowThreshold, lowThreshold*ratio, kernel_size);
+        imshow("Lee", dst);
+      }
+      else if(choice==51){
+        cvtColor(src, src_gray, CV_BGR2GRAY);
+        Mat copy;
+        copy=src.clone();
+        goodFeaturesToTrack(src_gray, corners, max_corners, quality_level, min_distance, Mat(), block_size, use_harris_detector, k);
+        cornerSubPix( src_gray, corners, win_size, zero_zone, criteria );
+        int r = 4;
+        for(int i=0; i<corners.size(); i++){
+          circle( copy, corners[i], r, Scalar(0,0,255), -1, 8, 0 );
+        }
+        imshow("Lee", copy);
+      }
+      else if(choice==52){
+        cvtColor(src, src_gray, CV_BGR2GRAY);
+        blur(src_gray, src_gray, Size(3,3));
+        Canny(src_gray, dst, lowThreshold, lowThreshold*ratio, kernel_size);
+        // cvtColor(dst, cdst, CV_GRAY2BGR);
+        vector<Vec2f> lines;
+        HoughLines(dst, lines, 1, CV_PI/180, threshold_line, 0, 0);
+        for( size_t i = 0; i < lines.size(); i++ ){
+           float rho = lines[i][0], theta = lines[i][1];
+           Point pt1, pt2;
+           double a = cos(theta), b = sin(theta);
+           double x0 = a*rho, y0 = b*rho;
+           pt1.x = cvRound(x0 + 1000*(-b));
+           pt1.y = cvRound(y0 + 1000*(a));
+           pt2.x = cvRound(x0 - 1000*(-b));
+           pt2.y = cvRound(y0 - 1000*(a));
+           line( src, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+        }
+        imshow("Lee", src);
+      }
+      else if(choice==53){
+        if(initial_flag == true){
+          src.copyTo(prev_frame);
+          initial_flag = false;
+        }
+        absdiff(prev_frame, src, dst);
+        src.copyTo(prev_frame);
+        imshow("Lee", dst);
+      }
+      else if(choice==113){
+        cout << "Terminating..." << endl;
+        break;
+      }
+      else{
+        imshow("Lee", src);
+      }
+    }
+    return 0;
+}
