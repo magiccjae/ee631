5b177563cafcdb4159db75f05644344c43faec90
Jae Lee <jaelee.byu@gmail.com>
2017-04-26 21:59:25 -0600

    visual odometry done

--- hw8/plotting.m
+++ hw8/plotting.m
@@ -1,6 +1,6 @@
 clear; close all; clc;
 
-truth = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/VO_Practice_Sequence/VO Practice Sequence R and T.txt');
+truth = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/VO Practice Sequence R and T.txt');
 
 Tx = truth(:,4);
 Tz = truth(:,12);
@@ -10,7 +10,7 @@ title('visual odometry');
 xlim([-300 300]);
 plot(Tx,Tz);
 
-vo = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/build/VO_result.txt');
+vo = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/VO_task1.txt');
 Tx_vo = vo(:,4);
 Tz_vo = vo(:,12);
 plot(Tx_vo, Tz_vo);
--- hw8/plotting2.m
+++ hw8/plotting2.m
@@ -2,10 +2,10 @@ clear; close all; clc;
 
 figure(1); hold on;
 title('visual odometry');
-xlim([-300 300]);
-ylim([-300 300]);
+xlim([-100 400]);
+ylim([-300 200]);
 
-vo = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/build/VO_result2.txt');
+vo = load('/home/magiccjae/jae_stuff/classes/ee631/hw8/VO_task2.txt');
 Tx_vo = vo(:,4);
 Tz_vo = vo(:,12);
 plot(Tx_vo, Tz_vo);
